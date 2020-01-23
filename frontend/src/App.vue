7a78de40ce7fd885348d4f2125a3d13d0915ce46
sam gaudet <samgaudet@samsMacmini.telus>
2020-01-23 13:24:11 -0700

    - added project files

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="main-app" class="container">
+        <span>Zero-emission, energy efficient spoon's are the futre of humanity.</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MainApp',
+        data: function() {
+            return {
+
+            }
+        }
+    }
+</script>
---
+++ frontend/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import App from './App.vue';
+
+import { library } from "@fortawesome/fontawesome-svg-core"
+import {
+    faPlus, faMinus, faTrash, faCheck
+} from "@fortawesome/free-solid-svg-icons"
+  
+library.add(faPlus, faMinus, faTrash, faCheck);
+
+Vue.config.productionTip = false;
+
+new Vue({
+    render: h => h(App),
+}).$mount('#app');
