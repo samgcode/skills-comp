32488479fcaa1b4234e99c84f6fc47f313023f2f
sam gaudet <samgaudet@samsMacmini.telus>
2020-01-24 15:14:43 -0700

    - initialized new project

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,23 @@
 <template>
-    <div id="main-app" class="container">
-        <span>Zero-emission, energy efficient spoon's are the futre of humanity.</span>
-    </div>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'MainApp',
-        data: function() {
-            return {
-
-            }
-        }
-    }
+export default {
+  name: 'App'
+}
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,15 @@
-import Vue from 'vue';
-import App from './App.vue';
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
 
-import { library } from "@fortawesome/fontawesome-svg-core"
-import {
-    faPlus, faMinus, faTrash, faCheck
-} from "@fortawesome/free-solid-svg-icons"
-  
-library.add(faPlus, faMinus, faTrash, faCheck);
+Vue.config.productionTip = false
 
-Vue.config.productionTip = false;
-
+/* eslint-disable no-new */
 new Vue({
-    render: h => h(App),
-}).$mount('#app');
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
