399c2fe98f19b1154b77c57b1e6307df8f58fdaf
sam gaudet <samgaudet@samsMacmini.telus>
2020-02-03 18:07:12 -0700

    - added operating hours card -

--- frontend/src/components/Contact.vue
+++ frontend/src/components/Contact.vue
@@ -1,45 +1,55 @@
 <template>
   <div class="container top-spacer">
     <h2>Contact us</h2>
-    <form class="text-center top-spacer">
-    <div class="form-row row">
-      <div class="form-group col-md-6">
-        <label for="nameInput">Name:</label>
-        <input 
-          class="form-control" 
-          type="text" 
-          name="nameInput" 
-          id="name-input"
-        >
+    <div class="row">
+      <form class="text-center top-spacer col-md-6">
+        <div class="form-row row">
+          <div class="form-group col">
+            <label for="nameInput">Name:</label>
+            <input 
+              class="form-control" 
+              type="text" 
+              name="nameInput" 
+              id="name-input"
+            >
+          </div>
+        </div>
+        <div class="form-row row">
+          <div class="form-group col">
+            <label for="emailInput">Email:</label>
+            <input 
+              class="form-control" 
+              type="email" 
+              name="emailInput" 
+              id="email-input"
+            >
+          </div>
+        </div>
+        <div class="form-row row">
+          <div class="form-group col">
+            <label for="emailInput">Message:</label>
+            <textarea 
+              class="form-control" 
+              id="message-input"
+              rows="5"
+            ></textarea>
+          </div>
+        </div>
+        <div class="container">
+          <div class="col-md-6">
+              <router-link :to="{ name: 'Home' }" class="lead btn btn-primary text-white">Send</router-link>
+          </div>
+        </div> 
+      </form>
+      <div class="card mb-4 shadow-sm col-md-5 top-spacer">
+        <div class="card-body">
+          <h3><u>Operating hours</u></h3>
+          <h5><u>mon-fri</u> 5am-10pm</h5>
+          <br>
+          <h5><u>sat-sun</u> 7am-8pm</h5>
+        </div>
       </div>
     </div>
-    <div class="form-row row">
-      <div class="form-group col-md-6">
-        <label for="emailInput">Email:</label>
-        <input 
-          class="form-control" 
-          type="email" 
-          name="emailInput" 
-          id="email-input"
-        >
-      </div>
-    </div>
-    <div class="form-row row">
-      <div class="form-group col-md-6">
-        <label for="emailInput">Message:</label>
-        <textarea 
-          class="form-control" 
-          id="message-input"
-          rows="5"
-        ></textarea>
-      </div>
-    </div>
-    <div class="container">
-      <div class="col-md-6">
-          <router-link :to="{ name: 'Home' }" class="lead btn btn-primary text-white">Send</router-link>
-      </div>
-    </div> 
-    </form>
   </div>
 </template>
 
--- frontend/src/components/Review.vue
+++ frontend/src/components/Review.vue
@@ -2,9 +2,9 @@
     <div class="col-md-5">
         <div class="card mb-4 shadow-sm">
             <div class="card-body">
-                <h4>{{ review.username }}</h4>
-                <p class="card-text">{{ review.review }}</p>
-                <span>Rating: {{ review.rating }}/5</span>
+              <h4>{{ review.username }}</h4>
+              <p class="card-text">{{ review.review }}</p>
+              <span>Rating: {{ review.rating }}/5</span>
             </div>
         </div>
     </div>
@@ -14,12 +14,5 @@
   export default {
     name: 'Review',
     props: ["review"],
-    // data: function() {
-    //   // return {
-    //   //   username: 'Jeff',
-    //   //   review: 'This is a review by a person that has a rating and this is a very good company yes yes.',
-    //   //   rating: 4
-    //   // }
-    // }
   }
 </script>
