a3560071c547f21eb1dbfce38cc1fca642e687bc
sam gaudet <samgaudet@sams-Mac-mini.telus>
2020-05-11 21:54:32 -0600

    - updated the about page with new content

---
+++ frontend/src/components/About/BenifitSection.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <hr class="featurette-divider top-spacer bottom-spacer">
+        <div class="row">
+            <div class="col-lg-4 text-center">
+                <img src="@/assets/oil_drill_field_lg.jpg" alt="Image of one of our spoons" class="bd-placeholder-img rounded-circle" width="140" height="140">
+                <h2>Rapid for work</h2>
+                <p>
+                    There’s no vacation from being a leader and
+                    provider for your family. Late nights and hard 
+                    work are a way of life, so what are you doing to 
+                    stay healthy? The revitalization of fresh air is 
+                    remarkably effective.
+                </p>
+                <p>
+                    Having Rapid Air is like giving your mind and 
+                    body a shot of nature.
+                </p>
+            </div>
+
+            <div class="col-lg-4 text-center">
+                <img src="@/assets/person_writing.jpg" alt="Image of one of our spoons" class="bd-placeholder-img rounded-circle" width="140" height="140">
+                <h2>Rapid for students</h2>
+                <p>
+                    Excelling at studies requires a focused mind 
+                    and the energy to complete your studies. 
+                    But the stress of timelines and commitments 
+                    quickly drains your body, and performance 
+                    suffers.
+                </p>
+                <p>
+                    Your body will give you all the strength you 
+                    need if you feed it the right ingredients. Quality
+                    air is one ingredient you shouldn’t overlook.
+                </p>
+            </div>
+
+            <div class="col-lg-4 text-center">
+                <img src="@/assets/rainbowSpoons.jpg" alt="Image of one of our spoons" class="bd-placeholder-img rounded-circle" width="140" height="140">
+                <h2>Rapid for children</h2>
+                <p>
+                    Developing bodies need ideal ingredients to 
+                    grow optimally. Clean, healthy lungs are what 
+                    you want for active children to reach their 
+                    potential.
+                </p>
+                <p>
+                    Rapid Air is family safe, easy to use and the 
+                    design of our cans makes it easy to reuse as 
+                    often as you need.
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BenifitSection',
+}
+</script>
