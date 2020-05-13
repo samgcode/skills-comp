38e22503d2bf23bbaabab134df6092ba45e25f63
sam gaudet <samgaudet@sams-Mac-mini.telus>
2020-05-13 15:54:56 -0600

    - used stitch instead of the backend

--- frontend/src/services/itemService.js
+++ frontend/src/services/itemService.js
@@ -2,10 +2,23 @@ const axios = require('axios');
 
 const baseURL = require('../urlConfig');
 
+const {
+    Stitch,
+    RemoteMongoClient,
+    AnonymousCredential
+} = require('mongodb-stitch-browser-sdk');
+
+const client = Stitch.initializeDefaultAppClient('rapidair-gfjbn');
+
+const db = client.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas').db('test');
+
+client.auth.loginWithCredential(new AnonymousCredential());
+
 class ItemService {
     async getItems() {
-        const response = await axios.get(`http://${baseURL}/items`);
-        return response.data;
+        const response = await db.collection('items').find({}, { limit: 100}).asArray();
+        console.log(response);
+        return response;
     }
 }
 
