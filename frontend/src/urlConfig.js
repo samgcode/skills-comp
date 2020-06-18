314a8eea70d88527ce527c17019b3c372a0af178
sam gaudet <samgaudet@sams-Mac-mini.telus>
2020-06-17 18:39:34 -0600

    - made the frontend hit heroku

--- frontend/src/urlConfig.js
+++ frontend/src/urlConfig.js
@@ -1,7 +1,8 @@
 const host = 'localhost';
 const port = 3000;
 
-const baseURL = `${host}:${port}`;
+// const baseURL = `${host}:${port}`;
+const baseURL = `skills-comp-backend.herokuapp.com`;
 
 export default {
     baseURL
