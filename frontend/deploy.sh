c909c38ae66266c2f2f04892e819695fecf98768
sam gaudet <samgaudet@sams-Mac-mini.telus>
2020-06-18 15:22:36 -0600

    -added a deploy.sh

---
+++ frontend/deploy.sh
@@ -0,0 +1,15 @@
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# https://samgcode.github.io/skills-comp
+
+git push -f git@github.com:samgcode/skills-comp.git master:gh-pages
+
+cd -
